6523390494e9c5a907d24665cf07d227d3efc028
ahmed-saad-tanim <ahmedsaadtanim@gmail.com>
2025-08-19 19:48:50 +0600

    Implement running away button

--- Assets/Scripts/RunAwayButton.cs
+++ Assets/Scripts/RunAwayButton.cs
@@ -1,62 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
 
-public class RunAwayButtonTeleport : MonoBehaviour
+public class ButtonEvents : MonoBehaviour, IPointerEnterHandler
 {
-    [SerializeField] private RectTransform buttonRect;
-    [SerializeField] private float safeDistance = 100f; // pixels in world space
-    [SerializeField] private Camera uiCamera;
-
-    private RectTransform canvasRect;
-
-    private void Start()
+    [SerializeField] RectTransform btnRectTransform;
+    
+    //set limit for the button to teleport
+    private Vector2 windowMinRange = new(-500f, -800f);
+    private Vector2 windowMaxRange = new(1500f, 500f);
+    
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        if (buttonRect == null)
-            buttonRect = GetComponent<RectTransform>();
-
-        canvasRect = buttonRect.GetComponentInParent<Canvas>().GetComponent<RectTransform>();
-
-        if (uiCamera == null)
-            uiCamera = Camera.main;
+        btnRectTransform.anchoredPosition = GetRandomButtonPosition();
     }
 
-    private void Update()
+    private Vector2 GetRandomButtonPosition()
     {
-        // Mouse position in world space
-        Vector3 mouseWorldPos = uiCamera.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPos.z = 0f;
-
-        // Button world position
-        Vector3 buttonWorldPos = buttonRect.position;
-
-        Debug.Log(mouseWorldPos + " - " + buttonWorldPos + " - " + Vector2.Distance(buttonWorldPos, mouseWorldPos));
-        
-        if (Vector2.Distance(mouseWorldPos, buttonWorldPos) < safeDistance / uiCamera.pixelHeight * canvasRect.rect.height)
-        {
-            // Pick a random position inside the canvas (world space)
-            Vector3 randomWorldPos = GetRandomWorldPositionAwayFrom(mouseWorldPos);
-
-            // Convert world position to local position relative to button's parent
-            buttonRect.localPosition = buttonRect.parent.InverseTransformPoint(randomWorldPos);
-        }
-    }
-
-    private Vector3 GetRandomWorldPositionAwayFrom(Vector3 avoidPos)
-    {
-        // Canvas corners in world space
-        Vector3[] corners = new Vector3[4];
-        canvasRect.GetWorldCorners(corners);
-
-        Vector3 randomPos;
-        do
-        {
-            randomPos = new Vector3(
-                Random.Range(corners[0].x, corners[2].x),
-                Random.Range(corners[0].y, corners[2].y),
-                0f
-            );
-        }
-        while (Vector2.Distance(randomPos, avoidPos) < safeDistance * 1.5f);
-
-        return randomPos;
+        return new Vector2(Random.Range(windowMinRange.x, windowMaxRange.x), Random.Range(windowMinRange.y, windowMaxRange.y));
     }
 }
